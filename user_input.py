efccdc5d0cd90a2c798aeab7ad557f76e476730b
domcain <dom.cain39781@gmail.com>
2022-10-05 12:01:38 +0800

    minor fix

--- user_input.py
+++ user_input.py
@@ -15,12 +15,11 @@ def user_input():
         "Enter the Certainty interval of the Green Team. (e.g. [-0.1,0.1] or [-0.5,0.7]): "
     )
     print(
-        "Size of the Green Team:" + NumberOfGreenNodes + "\n",
+        "Size of the Green Team: " + NumberOfGreenNodes + "\n",
         "Probability of a connection between any given green player: " + ProbabilityOfConnection + "\n",
         "Number of agents in the Grey Team: " + NumberOfGreyAgents + "\n",
         "Proportion of Red Spies within the Grey Team: " + RedSpyProportion + "\n",
         "Certainty interval of the Green Team: " + Certainty + "\n"
     )
-
 
 user_input()
