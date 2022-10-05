f0bff4367857bc106e6c823bfe53059616bcc95b
domcain <dom.cain39781@gmail.com>
2022-10-05 11:23:56 +0800

    modified print statements

--- user_input.py
+++ user_input.py
@@ -1,3 +1,9 @@
+NumberOfGreenNodes = None
+ProbabilityOfConnection = None
+NumberOfGreyAgents = None
+RedSpyProportion = None
+Certainty = None
+
 def user_input():
     NumberOfGreenNodes = input("Enter the size of the Green Team: ")
     ProbabilityOfConnection = input(
@@ -5,15 +11,15 @@ def user_input():
     )
     NumberOfGreyAgents = input("Enter Number of agents in the Grey Team: ")
     RedSpyProportion = input("Enter the proportion of Red Spies within the Grey Team: ")
-    Uncertainty = input(
-        "Enter the Uncertainty interval of the Green Team. (e.g. [-0.1,0.1] or [-0.5,0.7]): "
+    Certainty = input(
+        "Enter the Certainty interval of the Green Team. (e.g. [-0.1,0.1] or [-0.5,0.7]): "
     )
     print(
-        NumberOfGreenNodes,
-        ProbabilityOfConnection,
-        NumberOfGreyAgents,
-        RedSpyProportion,
-        Uncertainty,
+        "Size of the Green Team:" + NumberOfGreenNodes + "\n",
+        "Probability of a connection between any given green player: " + ProbabilityOfConnection + "\n",
+        "Number of agents in the Grey Team: " + NumberOfGreyAgents + "\n",
+        "Proportion of Red Spies within the Grey Team: " + RedSpyProportion + "\n",
+        "Certainty interval of the Green Team: " + Certainty + "\n"
     )
 
 
